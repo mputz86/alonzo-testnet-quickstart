d92343b95668604465e1f9ff3c0071962e299b8b
George Flerovsky <george@flerovsky.ca>
2021-07-15 21:39:14 -0400

    Fix Exercise 3 for Alonzo White.

--- exercise_3/scripts/prepare_for_plutus_script.sh
+++ exercise_3/scripts/prepare_for_plutus_script.sh
@@ -46,7 +46,7 @@ prepare_for_plutus_script() {
     --argjson fixed_cost $fixed_cost \
     --argjson prices "$(cardano-cli query protocol-parameters --testnet-magic 7 | jq '.executionUnitPrices')" \
     --argjson budget "$min_execution_units" \
-    '{"Steps": ($prices.priceSteps * $budget.Steps), "Memory": ($prices.priceMemory * $budget.Memory)} | add')
+    '{"Steps": (($prices.priceSteps.numerator / $prices.priceSteps.denominator) * $budget.Steps), "Memory": (($prices.priceMemory.numerator / $prices.priceMemory.denominator) * $budget.Memory)} | add')
 
   min_redemption_cost=$(($min_execution_cost + $fixed_cost))
   echo Minimum Cost to Redeem: $min_redemption_cost
